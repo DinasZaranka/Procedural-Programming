41b5b50f7227233db7ed3795ee0cffabfaede2f5
Dinas Zaranka <dinas.zaranka@mif.stud.vu.lt>
2025-12-06 18:24:36 +0200

    Functioning 4th assignment program (waiting for final touches)

--- pratybos/uzd4/src/list.h
+++ pratybos/uzd4/src/list.h
@@ -5,30 +5,27 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-//Ciklinio dvipusio saraso mazgo struktura
 typedef struct Node{
     int value;
     struct Node *next;
     struct Node *prev;
 } Node;
 
-//Laiko rodykle i saraso pradzia
-typedef struct{
-    Node *head;
-} List;
+void createEmptyList(Node **head);
 
-void initList(List *l);
+void insertEnd(Node **head, int value);
 
-void insertEnd(List *l, int value);
+void printList(Node *head);
 
-//saraso spausdinimas rekursyvus
-void printList(const List *l);
+void deleteMax(Node **head);
 
-void deleteMax(List *l);
+void deleteList(Node **head);
 
-void deleteList(List *l);
+void checkDataFileName(char *inputName);
 
-void readFromFile(List *l, const char *fileName);
+void CheckOutputFileName(char *outputName);
+
+void getFileData(Node **head);
 
 
 
--- pratybos/uzd4/src/main.c
+++ pratybos/uzd4/src/main.c
@@ -1,11 +1,55 @@
+//LSP:2513680, dinas.zaranka@mif.stud.vu.lt 4 uzd 8 variantas
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "list.h"
+
+int userMenu(){
+    int userChoice;
+    printf("===============Programos meniu:===============\n");
+    printf("0 - Sukuria tuscia sarasa\n");
+    printf("1 - nuskaito duomenis is jusu nurodyto failo\n");
+    printf("2 - spausdina sarasa\n");
+    printf("3 - Ismeta is saraso didziausia elementa\n");
+    printf("4 - istrina visa sarasa\n");
+    printf("5 - iseina is programos\n");
+    printf("==============================================\n");
+    printf("Iveskite ka norite kad programa darytu: ");
+    while(1){
+    scanf("%d",&userChoice);
+    
+    if(userChoice < 0 || userChoice > 5){
+        printf("Jusu pasirinkimas netinka, bandykite dar karta : ");
+        continue;
+    }
+    return userChoice;
+    }
+}
 
 int main(){
-
-
-
+    Node *head=NULL;
+    int userChoice;
+    while(1){
+        userChoice = userMenu();
+        if(userChoice == 0){
+            createEmptyList(&head);
+        }
+        
+        if(userChoice == 1) {
+            getFileData(&head);
+        }
+        if(userChoice == 2) {
+            printList(head);
+        }
+        if(userChoice == 3){
+            deleteMax(&head);
+        }
+        if(userChoice == 4){
+            deleteList(&head);
+        }
+        if(userChoice == 5) break;
+    }
+    
 
     return 0;
 }
